9c205111113c0bc921213c6bb2883f8cc9b936b5
Jacob Andersson <jacobandersson__@hotmail.com>
2018-10-17 11:17:29 +0200

    db init

---
+++ JAMCarRental.Domain/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMCarRental.Domain
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public Car TheCar { get; set; }
+        public int CarId { get; set; }
+        public Customer TheCustomer { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ JAMCarRental.Domain/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMCarRental.Domain
+{
+    public class Car
+    {
+        public Car()
+        {
+            Bookings = new List<Booking>();
+        }
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Brand { get; set; }
+        public string Mark { get; set; }
+        public DateTime Year { get; set; }
+        public List<Booking> Bookings { get; set; }
+    }
+}
