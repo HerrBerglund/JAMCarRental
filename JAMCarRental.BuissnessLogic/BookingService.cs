543026128eac1de328cff25b8d1b39b3da8bf614
HerrBerglund <35253558+HerrBerglund@users.noreply.github.com>
2018-10-17 21:58:29 +0200

    buissness logic

--- JAMCarRental.BuissnessLogic/BookingService.cs
+++ JAMCarRental.BuissnessLogic/BookingService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JAMCarRental.Domain;
+using JAMCarRental.Data;
 
 namespace JAMCarRental.BuissnessLogic
 {
     public class BookingService
     {
+        private JamCarRentalContext inputUserData = new JamCarRentalContext();
+
+        public void Add(Booking booking)
+        {
+            inputUserData.Bookings.Add(booking);
+            inputUserData.SaveChanges();
+        }
     }
 }
--- JAMCarRental.BuissnessLogic/CustomerService.cs
+++ JAMCarRental.BuissnessLogic/CustomerService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JAMCarRental.Domain;
+using JAMCarRental.Data;
 
 namespace JAMCarRental.BuissnessLogic
 {
     public class CustomerService
     {
+        private JamCarRentalContext inputUserData = new JamCarRentalContext();
+
+        public void Add(Customer customer)
+        {
+            inputUserData.Customers.Add(customer);
+            inputUserData.SaveChanges();
+        }
     }
 }
